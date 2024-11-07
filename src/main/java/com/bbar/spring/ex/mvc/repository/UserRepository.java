f8c787348972925b39585d4c4a78c99b9de3614a
moon777 <myj2139@gmail.com>
2024-11-07 16:07:25 +0900

    [문영주] - Mybatis MVC cycle 예제

---
+++ src/main/java/com/bbar/spring/ex/mvc/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.bbar.spring.ex.mvc.repository;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.bbar.spring.ex.mvc.domain.User;
+
+@Mapper
+public interface UserRepository {
+	
+	public int insertUser(
+			@Param("name") String name
+			,@Param("birthday") String birthday
+			,@Param("email") String email
+			,@Param("introduce") String introduce);
+	
+	public User selectLastUser();
+}
